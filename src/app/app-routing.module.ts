337b0f2523b5b90912c8953fd111a2e8b6efaf58
jonasmarcelo <marcelo.jonas@yahoo.com.br>
2022-03-08 22:49:03 -0300

    Criando o exemplo com property binding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'data-binding', component: DataBindingComponent },
+  { path: 'cursos', component: DataBindingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,15 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent implements OnInit {
 
-  url: string = 'https://agilizeodonto.com.br'
+  url: string = 'https://agilizeodonto.com.br';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'https://cdn.pixabay.com/photo/2017/10/10/07/48/hills-2836301_960_720.jpg';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getValor() {
+    return 1;
   }
 
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
